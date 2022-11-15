b0c8e7c6a265a3a33cbf254b02ecff94627c91c7
snatizzo <103608161+snatizzo@users.noreply.github.com>
2022-11-15 15:32:53 -0600

    Add files via upload

---
+++ .js
@@ -0,0 +1,11 @@
+function comenzarfunc()
+{
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier - ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/Sly5dh__Q/,modelReady');
+}
+function modelReady()
+ {
+    classifier.classify(gotResults);
+ }
+ function gotResults(error,results)
+ console.log(results)
